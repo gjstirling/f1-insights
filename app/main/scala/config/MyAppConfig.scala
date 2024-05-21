cceae1790b7afb464354191b02fdd35ce0e4eda1
Graeme Stirling <graemejstirling@gmail.com>
2024-05-21 15:30:43 +0100

    Filtering laps based on percentage and viability (elimate slow or invalid laps) - creating hotlaps

--- app/main/scala/config/MyAppConfig.scala
+++ app/main/scala/config/MyAppConfig.scala
@@ -8,4 +8,6 @@ class MyAppConfig() {
 
   val database = "f1insights"
   val eventsCollection = "events"
+
+  val hotLapCutOffPercentage = 1.02 // 2% cut off for valid laps
 }
--- app/main/scala/controllers/QualifyingLapsController.scala
+++ app/main/scala/controllers/QualifyingLapsController.scala
@@ -9,9 +9,10 @@ import main.scala.models.{LapData, QualifyingLaps}
 import play.api.libs.json.Format.GenericFormat
 import play.api.libs.json.OFormat.oFormatFromReadsAndOWrites
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import javax.inject._
 import services.Services
+
 
 @Singleton
 class QualifyingLapsController @Inject()(implicit executionContext: ExecutionContext,
@@ -20,22 +21,42 @@ class QualifyingLapsController @Inject()(implicit executionContext: ExecutionCon
                                          val f1Api: F1OpenApiController,
                                          config: MyAppConfig) extends BaseController {
 
+  private def sortAndFilterLaps(laps: List[QualifyingLaps]): List[QualifyingLaps] = {
+    val sortedLaps = laps.sortBy(_.lap_duration.getOrElse(Double.MaxValue))
+    val fastestLapOpt = sortedLaps.headOption.flatMap(_.lap_duration)
+
+    fastestLapOpt match {
+      case Some(fastestLap) =>
+        val filteredLaps = sortedLaps.filter { lap =>
+          lap.lap_duration.exists(_ <= fastestLap * 1.02)
+        }
+        filteredLaps.sortBy(_.lap_number)
+
+      case None =>
+        List.empty[QualifyingLaps]
+    }
+  }
+
   def find: Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val params = Services.extractParams(request)
+    val params: Map[String, String] = Services.extractParams(request).toMap
     val route = "/laps"
 
-    f1Api.lookup[List[QualifyingLaps]](route, params).map {
-      case Right(listOfLaps) =>
-        implicit val eventRw: ReadWriter[LapData] = macroRW
-        val convertToLaps = listOfLaps.map { lap => LapData(lap_number = lap.lap_number, lap.duration_sector_1, lap.duration_sector_2, lap.duration_sector_3, lap.lap_duration )}
+    // check driver number and session key are valid from maps ???
+    // return a custom response
 
-        // Convert the list of maps to a JSON array
-        val jsonArray = convertToJsonArray(convertToLaps)
-        Ok(jsonArray)
+      f1Api.lookup[List[QualifyingLaps]](route, params).map {
+        case Right(listOfLaps) =>
+          val hotLaps = sortAndFilterLaps(listOfLaps)
+          implicit val eventRw: ReadWriter[LapData] = macroRW
+          val convertToLaps = hotLaps.map { lap => LapData(lap_number = lap.lap_number, lap.duration_sector_1, lap.duration_sector_2, lap.duration_sector_3, lap.lap_duration) }
 
-      case Left(errors) =>
-        MyLogger.red(s"Failed to parse: $errors")
-        BadRequest("[QualifyingLapsController][find]:     Error with request")
-    }
+          // Convert the list of maps to a JSON array
+          val jsonArray = convertToJsonArray(convertToLaps)
+          Ok(jsonArray)
+
+        case Left(errors) =>
+          MyLogger.red(s"Failed to parse: $errors")
+          BadRequest("[QualifyingLapsController][find]:     Error with request")
+      }
   }
 }
