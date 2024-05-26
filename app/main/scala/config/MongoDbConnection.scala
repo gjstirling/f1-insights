97d97ee5dbc0174db5bdd4f3fb999f42d1f2117b
Graeme Stirling <graemejstirling@gmail.com>
2024-05-26 12:28:47 +0100

    extact codec provider into repository

--- app/main/scala/config/MongoDbConnection.scala
+++ app/main/scala/config/MongoDbConnection.scala
@@ -1,22 +1,19 @@
 package main.scala.config
 
-import main.scala.models.Event
 import org.bson.codecs.configuration.CodecProvider
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
 import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala._
-import org.mongodb.scala.bson.codecs.Macros
 
 import javax.inject.Singleton
+import scala.reflect.ClassTag
 
 @Singleton
 class MongoDbConnection {
   private val client: MongoClient = MongoClient()
 
-  def connect[T](db: String, collection: String): MongoCollection[T] = {
-    val codec = Macros.createCodecProvider[T]()
-
-    val codecRegistry = fromRegistries(fromProviders(codec), DEFAULT_CODEC_REGISTRY)
+  def connect[T: ClassTag](db: String, collection: String, codecProvider: CodecProvider): MongoCollection[T] = {
+    val codecRegistry = fromRegistries(fromProviders(codecProvider), DEFAULT_CODEC_REGISTRY)
     val database: MongoDatabase = client.getDatabase(db).withCodecRegistry(codecRegistry)
     database.getCollection[T](collection)
   }
--- app/main/scala/repositories/EventRepository.scala
+++ app/main/scala/repositories/EventRepository.scala
@@ -8,6 +8,7 @@ import javax.inject.{Inject, Singleton}
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.Duration
 import main.scala.config.{MongoDbConnection, MyAppConfig}
+import org.mongodb.scala.bson.codecs.Macros
 import org.mongodb.scala.bson.conversions
 import org.mongodb.scala.model.Filters._
 import org.mongodb.scala.model._
@@ -16,8 +17,10 @@ import services.MyLogger
 @Singleton
 class EventRepository @Inject()(config: MyAppConfig, dbConnection: MongoDbConnection) {
 
+  private lazy val codec = Macros.createCodecProvider[Event]()
+
   def insertEvents(events: Seq[Event]): Unit = {
-    val collection = dbConnection.connect[Event](config.database, config.eventsCollection)
+    val collection = dbConnection.connect[Event](config.database, config.eventsCollection, codec)
     // Prepare a list of bulk write operations
     val bulkWrites = events.map { event =>
       val filter = Filters.eq("session_key", event.session_key)
@@ -33,7 +36,7 @@ class EventRepository @Inject()(config: MyAppConfig, dbConnection: MongoDbConnec
   }
 
   def find(filters: Map[String, String]): Future[Seq[Event]] = {
-    val collection = dbConnection.connect[Event](config.database, config.eventsCollection)
+    val collection = dbConnection.connect[Event](config.database, config.eventsCollection, codec)
 
 
     val filterCriteria = filters.map { case (key, value) => equal(key, value) }
