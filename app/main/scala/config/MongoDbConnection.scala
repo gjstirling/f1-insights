67d397c1ff1915aa7079e4485ff9ce2006b44263
Graeme Stirling <graemejstirling@gmail.com>
2024-05-30 15:09:20 +0100

    tidy

--- app/main/scala/config/MongoDbConnection.scala
+++ app/main/scala/config/MongoDbConnection.scala
@@ -7,26 +7,22 @@ import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala._
 
 import javax.inject.Singleton
-import scala.concurrent.Await
-import scala.concurrent.duration.DurationInt
 import scala.reflect.ClassTag
-import scala.util.Using
+
 
 @Singleton
 class MongoDbConnection {
-
+  // Mongo DB Atlas conf
   // Retrieve the password from the environment variables
   private val password: String = sys.env.getOrElse("PASSWORD", throw new IllegalStateException("PASSWORD environment variable is not set"))
 
-  val connectionString = s"mongodb+srv://graemejstirling:$password@mycluster.k53nho7.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster"  // Construct a ServerApi instance using the ServerApi.builder() method
+  val connectionString = s"mongodb+srv://graemejstirling:$password@mycluster.k53nho7.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster" // Construct a ServerApi instance using the ServerApi.builder() method
   val serverApi: ServerApi = ServerApi.builder.version(ServerApiVersion.V1).build()
   val settings: MongoClientSettings = MongoClientSettings
     .builder()
     .applyConnectionString(ConnectionString(connectionString))
     .serverApi(serverApi)
     .build()
-
-  // Mongo DB Atlas
   private val client: MongoClient = MongoClient(settings)
   // local db
   //private val client: MongoClient = MongoClient()
